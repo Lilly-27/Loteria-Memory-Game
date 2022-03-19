fb61bb64e97026ff7cfe02ffbb409b96eb64cb03
Lilly-27 <ghcecilia1@gmail.com>
2022-03-18 22:12:13 -0500

    Finished the README

--- README.md
+++ README.md
@@ -1,24 +1,36 @@
-I created a memory game inspired by the Latine board game, "Loteria," to celebrate my family's culture.
+# Loteria Memory Game
 
-USER STORIES
+This game was inspired by the Latinx game Cecilia grew up playing with her family called, "Loteria." Cecilia hopes to celebrate Latinx culture, and welcomes users to enjoy her game! 
 
-MVP GOALS
+## SCREENSHOTS
+---
+When the game opens, you will see this homepage: 
+![Homepage of Loteria Memory Game] (lib/screenshots/home.png)
 
-As a player, I want my game to recognize when I make a match, as well as when I made all possible matches.
+Once you click on "Jugar," then the cards will appear! 
+![Initial Start Page of Loteria Memory Game] (lib/screenshots/startpage.png)
 
-As a player, I want my game to accumulate points with each match. 
+##Technologies Used
+---
+Front-end: HTML5, CSS3, and vanilla JavaScript
 
-As a player, I want my game to recognize when I did not make a match.
+## GAME PREMISE
+---------------
 
-As a player, I want to be informed of how much time I have left.
+- Users are greeted with the opening page of the game. They need to click on "Jugar" (Spanish for "play") to start the game. 
 
-As a player, I want to be congratulated when I win.
+- Users click on the image they would like to flip. For each pair they find, users are alerted that they earned 20 points. The cards then disappear, leaving behind an empty space to mark where the cards used to be. 
 
-As a player, I want to restart the game after a win or a loss.
+- Users lose 5 points if they do not make a match. They are alerted when they do not make a match, and the cards immediately flip back to their place. 
 
+- Once users find all possible matches, their success is celebrated with a message that pops up that congratulates them. They are then invited to play again. If users click on "play again," the game restarts to the beginning and the cards are shuffled. 
 
-STRETCH GOALS
+### What's Next?
+- Include a musical timer that shuffles through popular Latinx songs.
+- Improve code to be DRY-er. 
+- Include the final score in the modal message. 
+- Improve responsiveness.
+- Improve accessibilities to make the game an enjoyable experience for everyone.
 
-As a player, I want to listen to music as I play. Once the timer is done, the music stops and shows me a screen that says, "Game Over!"
-
-As a player, I want my game to deduct points if I selected a wrong card. I do not want my game to go to the negatives. 
+### Known Bugs?
+- Game is best used when it takes up the entire computer screen.
--- start.js
+++ start.js
@@ -9,17 +9,17 @@ const match = []
 // Creating an array of objects with all my cards:
 
     const cardsArray = [
-        {name: 'El-Nopal', img: "cards/el_nopal.png"},{name: 'El-Nopal', img: "cards/el_nopal.png"},
+        {name: 'El-Nopal', img: "lib/cards/el_nopal.png"},{name: 'El-Nopal', img: "lib/cards/el_nopal.png"},
     
-        {name: 'El-Pescado', img: "cards/el_pescado.png"},{name: 'El-Pescado', img: "cards/el_pescado.png"},
+        {name: 'El-Pescado', img: "lib/cards/el_pescado.png"},{name: 'El-Pescado', img: "lib/cards/el_pescado.png"},
     
-        {name: 'La-Calavera', img: "cards/la_calavera.png"},{name: 'La-Calavera', img: "cards/la_calavera.png"},
+        {name: 'La-Calavera', img: "lib/cards/la_calavera.png"},{name: 'La-Calavera', img: "lib/cards/la_calavera.png"},
     
-        {name: 'La-Luna', img: "cards/la_luna.png"},{name: 'La-Luna', img: "cards/la_luna.png"},
+        {name: 'La-Luna', img: "lib/cards/la_luna.png"},{name: 'La-Luna', img: "lib/cards/la_luna.png"},
     
-        {name: 'La-Rosa', img: "cards/la_rosa.png"},{name: 'La-Rosa', img: "cards/la_rosa.png"},
+        {name: 'La-Rosa', img: "lib/cards/la_rosa.png"},{name: 'La-Rosa', img: "lib/cards/la_rosa.png"},
     
-        {name: 'La-Sandia', img: "cards/la_sandia.png"},{name: 'La-Sandia', img: "cards/la_sandia.png"},
+        {name: 'La-Sandia', img: "lib/cards/la_sandia.png"},{name: 'La-Sandia', img: "lib/cards/la_sandia.png"},
     ]
 
 // Creating my board, adding a data
@@ -28,7 +28,7 @@ function createBoard () {
     for (let i=0; i < cardsArray.length; i++) {
         const card = document.createElement("img");
         card.classList.add('empty-space')
-        card.setAttribute('src', 'cards/back_card.png');
+        card.setAttribute('src', 'lib/cards/back_card.png');
         card.setAttribute('data-id', i)
         card.addEventListener('click', flipCard);
         // console.log(grid)
@@ -58,15 +58,15 @@ function checkMatch () {
     const secondOptionId = cardsChosenId[1]
     if (cardsChosen[0].getAttribute('src') === cardsChosen[1].getAttribute('src')) {
         alert("Yes, it's a match! You earned 20 points!")
-        cards[firstOptionId].setAttribute('src', 'cards/blank_space.png')
-        cards[secondOptionId].setAttribute('src', 'cards/blank_space.png')
+        cards[firstOptionId].setAttribute('src', 'lib/cards/blank_space.png')
+        cards[secondOptionId].setAttribute('src', 'lib/cards/blank_space.png')
         match.push(cardsChosen[0])
         match.push(cardsChosen[1])
         result.innerText = parseInt(result.innerText) + 20
         win()
     } else {
-        cards[firstOptionId].setAttribute('src', 'cards/back_card.png')
-        cards[secondOptionId].setAttribute('src', 'cards/back_card.png')
+        cards[firstOptionId].setAttribute('src', 'lib/cards/back_card.png')
+        cards[secondOptionId].setAttribute('src', 'lib/cards/back_card.png')
         cardsChosen.forEach((card) => card.addEventListener('click', flipCard))
         result.innerText = parseInt(result.innerText) - 5
         alert ("Not a match. You lost 5 points. Try again!")
